71e151fc7d3567826685a964c695b17627c97b78
m110100 <shindayoni@gmail.com>
2025-04-28 01:41:32 +0300

    fix: add default api url cause gh-pages doesnt recognize .env :-)

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiCaller = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: import.meta.env.VITE_API_URL || 'https://api.thecatapi.com/v1',
 	headers: {
 		'Content-Type': 'application/json',
 		'Accept': 'application/json',
